e7be01cad4c570c49fb374d14d5c03b84b51c40d
Jean-Marc Pelletier <jmp@jmpelletier.com>
2023-05-14 21:13:18 +0900

    マスクトランジション実装

--- Scripts/CanvasTransition.cs
+++ Scripts/CanvasTransition.cs
@@ -1,21 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace PuzzleBox
 {
+    [ExecuteAlways]
     [RequireComponent(typeof(CanvasGroup))]
+    [RequireComponent(typeof(Image))]
     public class CanvasTransition : MonoBehaviour
     {
         public float inTransitionTime = 0.2f;
         public float outTransitionTime = 0.2f;
+        public float maskScale = 1f;
 
         Animator animationController;
+
+        Image mask;
+        Material maskMaterial;
 
         // Start is called before the first frame update
         void Start()
         {
             animationController = GetComponent<Animator>();
+        }
+
+        private void LateUpdate()
+        {
+            if (mask == null)
+            {
+                mask = GetComponent<Image>();
+            }
+
+            if (mask != null && mask.IsActive())
+            {
+                float currentScale = mask.material.GetFloat("_Scale");
+                if (currentScale != maskScale)
+                {
+                    mask.materialForRendering.SetFloat("_Scale", maskScale);
+                }
+            }
         }
 
         public void Show()
