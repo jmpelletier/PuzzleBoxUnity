8403fad0806db4f98e3c91146ee44727a8080a5c
Jean-Marc Pelletier <jmp@jmpelletier.com>
2023-05-15 21:05:09 +0900

    トリガー関連アセット追加

---
+++ Scripts/DestroyZone2D.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PuzzleBox
+{
+    public class DestroyZone2D : MonoBehaviour
+    {
+        public string targetTag = "";
+        public LayerMask layerMask = ~0;
+        public bool destroyOnExit = false;
+        public UnityEvent OnDestroy;
+        
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (!destroyOnExit && (targetTag == "" || collision.tag == targetTag) && (layerMask.value & (1 << collision.gameObject.layer)) > 0)
+            {
+                OnDestroy?.Invoke();
+                Destroy(collision.gameObject);
+            }
+            
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (destroyOnExit && (targetTag == "" || collision.tag == targetTag) && (layerMask.value & (1 << collision.gameObject.layer)) > 0)
+            {
+                OnDestroy?.Invoke();
+                Destroy(collision.gameObject);
+            }
+
+        }
+    }
+}
+
---
+++ Scripts/TriggerActions.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PuzzleBox
+{
+    public class TriggerActions : MonoBehaviour
+    {
+        public string targetTag = "";
+        public LayerMask layerMask = ~0;
+        public UnityEvent onEnter;
+        public UnityEvent onExit;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if ((targetTag == "" || collision.tag == targetTag) && (layerMask.value & (1 << collision.gameObject.layer)) > 0)
+            {
+                onEnter?.Invoke();
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if ((targetTag == "" || collision.tag == targetTag) && (layerMask.value & (1 << collision.gameObject.layer)) > 0)
+            {
+                onExit?.Invoke();
+            }
+        }
+    }
+}
+
