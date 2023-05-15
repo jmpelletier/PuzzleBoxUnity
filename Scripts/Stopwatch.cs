3e482bece2fe92cec8d3fe507cfe86abbe7acb02
Jean-Marc Pelletier <jmp@jmpelletier.com>
2023-05-15 21:04:35 +0900

    タイマー追加

---
+++ Scripts/Stopwatch.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PuzzleBox
+{
+    public class Stopwatch : MonoBehaviour
+    {
+
+        [Header("Stopwatch")]
+        public bool active = false;
+        public bool useFixedUpdate = false;
+
+        public UnityEvent OnStart;
+        public UnityEvent<float> OnUpdate;
+        public UnityEvent<bool> OnPause;
+
+        public float time { get; protected set; }
+
+        private void Start()
+        {
+            if (active)
+            {
+                StartTimer();
+            }
+        }
+
+        public virtual void StartTimer()
+        {
+            active = true;
+            time = 0;
+            OnStart?.Invoke();
+        }
+
+        public void PauseTimer(bool paused)
+        {
+            if (active == paused)
+            {
+                active = !paused;
+                OnPause?.Invoke(paused);
+            }
+        }
+
+        protected virtual void UpdateTime(float deltaTime)
+        {
+            if (active)
+            {
+                time += deltaTime;
+                OnUpdate?.Invoke(time);
+            }
+        }
+
+        private void Update()
+        {
+            if (!useFixedUpdate)
+            {
+                UpdateTime(Time.deltaTime);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (useFixedUpdate)
+            {
+                UpdateTime(Time.fixedDeltaTime);
+            }
+        }
+    }
+}
+
---
+++ Scripts/Timer.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PuzzleBox
+{
+    public class Timer : Stopwatch
+    {
+        public enum CountDirection
+        {
+            Up,
+            Down
+        }
+
+        [Header("Timer")]
+        public float duration = 10f;
+        public CountDirection countDirection = CountDirection.Down;
+
+        public UnityEvent OnEnd;
+
+        public override void StartTimer()
+        {
+            base.StartTimer();
+
+            if (countDirection == CountDirection.Down)
+            {
+                time = duration;
+            }
+            else
+            {
+                time = 0;
+            }
+        }
+
+        protected override void UpdateTime(float deltaTime)
+        {
+            if (active)
+            {
+                if (countDirection == CountDirection.Down)
+                {
+                    time -= deltaTime;
+                    if (time <= 0)
+                    {
+                        time = 0;
+                        active = false;
+                        OnEnd?.Invoke();
+                        return;
+                    }
+                }
+                else
+                {
+                    time += deltaTime;
+                    if (time >= duration)
+                    {
+                        time = duration;
+                        active = false;
+                        OnEnd?.Invoke();
+                        return;
+                    }
+                }
+
+                OnUpdate?.Invoke(time);
+            }
+        }
+    }
+}
+
