6208c7ef57e88b0b18d3a045da5d21417089c664
Jean-Marc Pelletier <jmp@jmpelletier.com>
2024-04-22 18:11:10 +0900

    New Instance method for calling methods exposed with Action attribute, also hide gizmo option.

--- Runtime/Scripts/Core/PuzzleBoxBehaviour.cs
+++ Runtime/Scripts/Core/PuzzleBoxBehaviour.cs
@@ -7,11 +7,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Reflection;
 
 namespace PuzzleBox
 {
     public abstract class PuzzleBoxBehaviour : MonoBehaviour
     {
+        public bool hideGizmo = false;
+
+        private static Dictionary<string, MethodInfo[]> _actionMethodInfo = new Dictionary<string, MethodInfo[]>();
+        private static void RegisterInstanceActions(PuzzleBoxBehaviour instance)
+        {
+            string type = instance.GetType().Name;
+            if (!_actionMethodInfo.ContainsKey(type))
+            {
+                _actionMethodInfo[type] = ActionAttribute.GetMethods(instance.GetType());
+            }
+
+            foreach(MethodInfo method in _actionMethodInfo[type])
+            {
+                instance._actions[method.Name] = (Action)Delegate.CreateDelegate(typeof(Action), instance, method);
+            }
+        }
+
+        protected Dictionary<string, Action> _actions { get; private set; } = new Dictionary<string, Action>();
+
+        public PuzzleBoxBehaviour() : base()
+        {
+            RegisterInstanceActions(this);
+        }
+         
+
         // Start is called before the first frame update
         void Start()
         {
@@ -36,6 +63,30 @@ namespace PuzzleBox
         public virtual bool GetToggleState()
         {
             return isActiveAndEnabled;
+        }
+
+        [PuzzleBox.Action]
+        public void Enable()
+        {
+            enabled = true;
+        }
+
+        [PuzzleBox.Action]
+        public void Disable()
+        {
+            enabled = false;
+        }
+
+        public virtual void Invoke(string message)
+        {
+            if (_actions.ContainsKey(message))
+            {
+                Action action = _actions[message];
+                if (action != null)
+                {
+                    action.Invoke();
+                }
+            }
         }
 
     }
