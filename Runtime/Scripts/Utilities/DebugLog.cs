e91149a74d22034651caf594c5fee77ced83e7e0
Jean-Marc Pelletier <jmp@jmpelletier.com>
2024-04-30 23:00:04 +0900

    Added DebugLog for printing to console

---
+++ Runtime/Scripts/Utilities/DebugLog.cs
@@ -0,0 +1,24 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PuzzleBox
+{
+    public class DebugLog : PuzzleBoxBehaviour
+    {
+        public string message;
+
+        [PuzzleBox.Action]
+        public void Print()
+        {
+            Debug.Log(message);
+        }
+    }
+}
+
