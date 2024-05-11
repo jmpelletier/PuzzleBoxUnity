eaa4c62f996b45105a605688677730de13490ffb
Jean-Marc Pelletier <jmp@jmpelletier.com>
2024-05-11 17:59:45 +0900

    Fixed prefab

--- Runtime/Scripts/ActionDelegates/SendMessage.cs
+++ Runtime/Scripts/ActionDelegates/SendMessage.cs
@@ -24,38 +24,18 @@ namespace PuzzleBox
         public string message = "";
         public MessageTarget[] targets;
         public GameObject[] arguments;
-        
 
         public override void Perform(GameObject sender)
         {
-            foreach(MessageTarget target in targets)
+            foreach (MessageTarget target in targets)
             {
                 if (target.behaviour != null)
                 {
                     PuzzleBoxBehaviour behaviour = target.behaviour;
                     PerformAction(() => behaviour.Invoke(message, sender, arguments));
-                    //if (delay > 0)
-                    //{
-                    //    StartCoroutine(InvokeWithDelay(target.behaviour, sender, delay));
-                    //}
-                    //else
-                    //{
-                    //    target.behaviour.Invoke(message, sender, arguments);
-                    //}
-                    
                 }
             }
         }
-
-        //private IEnumerator InvokeWithDelay(PuzzleBoxBehaviour targetBehaviour, GameObject sender, float delay)
-        //{
-        //    if (delay > 0)
-        //    {
-        //        yield return new WaitForSeconds(delay);
-        //    }
-
-        //    targetBehaviour.Invoke(message, sender, arguments);
-        //}
 
         public override string GetIcon()
         {
